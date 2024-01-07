4fb7e99b2af2a500daf37a72bc333fcbb343661e
hakim2999 <49701397+hakim2999@users.noreply.github.com>
2024-01-08 00:21:33 +0200

    update header

--- src/components/TodoHeader/TodoHeader.jsx
+++ src/components/TodoHeader/TodoHeader.jsx
@@ -8,7 +8,9 @@ const TodoHeader = ({ darkMode, onThemeToggle }) => {
       <button className="icon" onClick={onThemeToggle}>
         <img
           id="icon-image"
-          src={darkMode ? "/assets/icons/dark-mode.svg" : "/assets/icons/light-mode.svg"}
+          // src={darkMode ? "/assets/icons/dark-mode.svg" : "/assets/icons/light-mode.svg"}
+          src={darkMode ? "vscode-vfs://github%2B7b2276223a312c22726566223a7b2274797065223a352c226964223a2267682d7061676573227d7d/hakim2999/Todo-List/assets/icons/dark-mode.svg" : "/assets/icons/light-mode.svg"}
+
           alt="theme-mode"
         />
       </button>
